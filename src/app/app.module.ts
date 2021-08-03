4234d22a6e9baa17aab2e1297d62856e06aa34f0
Nike Starbrink <nike.starbrink@umu.se>
2021-08-03 17:03:41 +0200

    lägger till om mig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectPrevComponent } from './project-prev/project-prev.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectPrevComponent
+    ProjectPrevComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path : 'home', component: ProjectPrevComponent},
+      { path : '', redirectTo: 'home', pathMatch: 'full'},
+      { path : '**', redirectTo: 'home', pathMatch: 'full'}
+  ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
